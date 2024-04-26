3c8d86cb81e7d9348d2beae938d037ab8e8dcbb9
ปรเมษ แก้วอุบล <keawubolporamet505@gmail.com>
2024-04-26 22:18:43 +0700

    carousel formid page

--- client/src/pages/form/[formId].tsx
+++ client/src/pages/form/[formId].tsx
@@ -1,119 +1,154 @@
-import Navbar from '@/components/Navbar';
-import conf from '@/conf/main';
-import { Form } from '@/modules/form';
-import { SingleForm } from '@/modules/singleForm';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import { PDFDocument, StandardFonts, degrees, rgb } from 'pdf-lib';
-import React, { useEffect, useState } from 'react'
-import fontkit from '@pdf-lib/fontkit';
-import { useAuth } from 'react-oidc-context';
-import { Route } from '@/modules/routes';
-import PreviewModal from '@/components/PreviewModal';
+import Navbar from "@/components/Navbar";
+import conf from "@/conf/main";
+import { Form } from "@/modules/form";
+import { SingleForm } from "@/modules/singleForm";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { PDFDocument, StandardFonts, degrees, rgb } from "pdf-lib";
+import React, { useEffect, useState } from "react";
+import fontkit from "@pdf-lib/fontkit";
+import { useAuth } from "react-oidc-context";
+import { Route } from "@/modules/routes";
+import PreviewModal from "@/components/PreviewModal";
+import Footer from "@/components/Footer";
+import { Carousel } from "flowbite-react";
+import Image from "next/image";
+import { string } from "zod";
 
 function FormIdPage() {
-    const router = useRouter();
-    const auth = useAuth();
-    const formId = router.query.formId;
-    const [form, setForm] = useState<SingleForm>();
-    const [studentDetail, setStudentDetail] = useState<any>(null);
-    const [pdfUrl, setPdfUrl] = useState<string>()
-    const [openModal, setOpenModal] = useState<boolean>(false);
-    console.log(studentDetail);
-    
+  const router = useRouter();
+  const auth = useAuth();
+  const formId = router.query.formId;
+  const [form, setForm] = useState<SingleForm>();
+  const [studentDetail, setStudentDetail] = useState<any>(null);
+  const [pdfUrl, setPdfUrl] = useState<string>();
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  console.log(studentDetail);
 
-    const fetchForm = async () => {
-        try {
-            const listForms = await axios.get(`${conf.urlPrefix}/forms/${formId}`);
-            setForm(listForms.data);
-        } catch (error) {
-            console.error(error);
-
-        }
-    };
-
-    const fectStudentDetail = async () => {
-        const result = await axios.get(
-            `${conf.urlPrefix}/psu-api/studentDetail`,
-            {
-                headers: {
-                    token: auth.user?.access_token,
-                },
-            }
-        );
-        setStudentDetail(result.data.data[0]);
+  const fetchForm = async () => {
+    try {
+      const listForms = await axios.get(`${conf.urlPrefix}/forms/${formId}`);
+      setForm(listForms.data);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-        async function modifyPdf() {
-            try {
-                const url = `${conf.urlPrefix}/forms${form?.pdfURL}`;
-                const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
+  const fectStudentDetail = async () => {
+    const result = await axios.get(`${conf.urlPrefix}/psu-api/studentDetail`, {
+      headers: {
+        token: auth.user?.access_token,
+      },
+    });
+    setStudentDetail(result.data.data[0]);
+  };
 
-                const pdfDoc = await PDFDocument.load(existingPdfBytes);
-                const fontUrl = 'https://raw.githubusercontent.com/PacharaponK/PSU_OpenAPI_Contest/main/uploads/THSarabunNew.ttf';
-                const fontBytes = await fetch(fontUrl).then(res => res.arrayBuffer());
+  async function modifyPdf() {
+    try {
+      const url = `${conf.urlPrefix}/forms${form?.pdfURL}`;
+      const existingPdfBytes = await fetch(url).then((res) =>
+        res.arrayBuffer()
+      );
 
-                pdfDoc.registerFontkit(fontkit);
-                const customFont = await pdfDoc.embedFont(fontBytes);
+      const pdfDoc = await PDFDocument.load(existingPdfBytes);
+      const fontUrl =
+        "https://raw.githubusercontent.com/PacharaponK/PSU_OpenAPI_Contest/main/uploads/THSarabunNew.ttf";
+      const fontBytes = await fetch(fontUrl).then((res) => res.arrayBuffer());
 
-                const pages = pdfDoc.getPages();
-                const modifyPage = pages[1]; // config
-                const { width, height } = modifyPage.getSize();
+      pdfDoc.registerFontkit(fontkit);
+      const customFont = await pdfDoc.embedFont(fontBytes);
 
-                modifyPage.drawText(studentDetail.titleNameThai+studentDetail.studNameThai+" "+studentDetail.studSnameThai, {
-                    x: 237.56,
-                    y: (height / 2) + 95,
-                    size: 15,
-                    font: customFont,
-                    color: rgb(0, 0, 0),
-                }); //config
+      const pages = pdfDoc.getPages();
+      const modifyPage = pages[1]; // config
+      const { width, height } = modifyPage.getSize();
 
-                const pdfBytes = await pdfDoc.save()
-                const blob = new Blob([pdfBytes], { type: "application/pdf" });
-                const modifiedPdfUrl = URL.createObjectURL(blob);
-                setPdfUrl(modifiedPdfUrl);
-                setOpenModal(true);
-                // const link = document.createElement("a");
-                // link.href = modifiedPdfUrl;
-                // link.download = "modified_pdf.pdf";
-                // link.click();
-                // URL.revokeObjectURL(url);
-            } catch (error) {
-                console.log(error);
-            }
+      modifyPage.drawText(
+        studentDetail.titleNameThai +
+          studentDetail.studNameThai +
+          " " +
+          studentDetail.studSnameThai,
+        {
+          x: 237.56,
+          y: height / 2 + 95,
+          size: 15,
+          font: customFont,
+          color: rgb(0, 0, 0),
         }
+      ); //config
 
-        useEffect(() => {
-            fetchForm();
-            fectStudentDetail();
-        }, [router.isReady])
-
-        console.log(form);
-
-
-        return (
-            <div>
-                <Navbar />
-                <div className='h-screen background-image flex flex-col justify-center items-center space-y-4'>
-                    <h1 className='pl-4 font-bold text-3xl'>{form?.name}</h1>
-                    <div className='flex justify-center items-center w-1/2 bg-cyan-100 p-5 rounded-xl'>
-                        <div className='text-left'>
-                            <p className='text-center text-xl pb-4'></p>
-                            <span className='whitespace-pre-line'>{form?.detail}</span>
-                        </div>
-                    </div>
-                    <button
-                        onClick={() => modifyPdf()}
-                        type="button"
-                        className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                    >
-                        ดาวน์โหลดฟอร์ม
-                    </button>
-                    <PreviewModal pdfUrl={pdfUrl} openModal={openModal} setOpenModal={setOpenModal} />
-                </div>
-                <iframe title="pdfViewer" src={pdfUrl} width="100%" height="500px" frameBorder="0"></iframe>
-            </div>
-        )
+      const pdfBytes = await pdfDoc.save();
+      const blob = new Blob([pdfBytes], { type: "application/pdf" });
+      const modifiedPdfUrl = URL.createObjectURL(blob);
+      setPdfUrl(modifiedPdfUrl);
+      setOpenModal(true);
+      // const link = document.createElement("a");
+      // link.href = modifiedPdfUrl;
+      // link.download = "modified_pdf.pdf";
+      // link.click();
+      // URL.revokeObjectURL(url);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    export default FormIdPage
+  useEffect(() => {
+    fetchForm();
+    fectStudentDetail();
+  }, [router.isReady]);
+
+  console.log(form);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="h-screen background-image flex flex-col justify-center items-center space-y-4 pt-20">
+        <h1 className="pl-4 font-bold text-3xl">{form?.name}</h1>
+        {form?.picDetailURL && form.picDetailURL.length > 0 && (
+          <div className="size-6/12 flex justify-center items-center">
+            <Carousel style={{ width: "100%", height: "100%" }}>
+              {form.picDetailURL.map((url, index) => (
+                <img
+                  key={index}
+                  src={url}
+                  alt={`${index + 1}`}
+                  style={{ objectFit: "cover", width: "100%", height: "100%" }}
+                />
+              ))}
+            </Carousel>
+          </div>
+        )}
+        <div className="flex justify-center items-center w-1/2 bg-cyan-100 p-5 rounded-xl">
+          <div className="text-left">
+            ขั้นตอนดำเนินการ
+            <p className="text-center text-xl pb-4"></p>
+            <span className="whitespace-pre-line">{form?.detail}</span>
+          </div>
+        </div>
+        <button
+          onClick={() => modifyPdf()}
+          type="button"
+          className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          ดาวน์โหลดฟอร์ม
+        </button>
+        <PreviewModal
+          pdfUrl={pdfUrl}
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+        />
+      </div>
+      {/* <iframe
+        title="pdfViewer"
+        src={pdfUrl}
+        width="100%"
+        height="500px"
+        frameBorder="0"
+      ></iframe> */}
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default FormIdPage;
